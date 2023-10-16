3e0214d97f7bff26c6fcd7b139c6669250b1fcbb
Daniel-144 <dani0142004@gmail.com>
2023-10-16 22:40:48 +0530

    internship questions

--- internship/words between the.py
+++ internship/words between the.py
@@ -7,26 +7,28 @@ Eg The king went to the forest with the wife and a servernt. The king shot a dee
 
 answer is 4 (The king, the forest, The King the next).
 """
-# get the passage as a input form the user.
-# split the word
-def findA(counter,i,passage):
-    for k in range(i,counter+1):
-        if passage[k]=="a":
-            return 0
-        break
-    return 1
-        
-
-passage=input("enter the passage")
+passage="The king went to the forest with the wife and a servernt. The king shot a deer. The king went to the forest again the next day."
+# changed the passage to lower case to make the string case insensitive
 passage=passage.lower()
-passage=passage.split(" ")
-print(passage)
+# used stip function to strip the passage with the word "the" ex: he is the hero :[he is,hero]
+print("the passage is:",passage)
+passage=passage.split("the")
+# if there is a the present as the first word then empty character will be appended in a string so we use remove to remove the empty chracter
+passage.remove("")
 count=0
-for i in range(len(passage) - 1):
-     for j in range(len(passage)):
-         print (passage[i],passage[j])
-         if passage[i]=="the" and passage[j]=="the":
-             counter=j-i
-             count+=findA(counter,i,j,passage)
-print(count)
-           
+# loop to iterate through the list
+for i in range(len(passage)):
+    #  conditional statement to find if there is a "a" present in the string
+    if "a" not in passage[i]:
+        # increment count
+        count+=1
+# output
+print("the answer is:",count)
+# the next day will not be taken into count because the word day has a "a" in it
+
+"""
+OP
+the passage is: the king went to the forest with the wife and a servernt. the king shot a deer. the king went to the forest again the next day.
+the answer is: 3
+"""
+
